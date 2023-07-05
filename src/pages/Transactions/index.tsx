e06f5099ba6c2155c2ae27eb2239c94ab360c6c8
YanCardoso <yancard7@gmail.com>
2023-07-04 23:12:38 -0300

    table template

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import {
+	PriceHighlight,
+	TransactionsTable,
+	TransactionsTableContainer,
+} from './styles'
 
 export function Transactions() {
 	return (
 		<div>
 			<Header />
 			<Summary />
+
+			<TransactionsTableContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td>Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>13/04/2023</td>
+						</tr>
+						<tr>
+							<td>Aluguel do apartamento</td>
+							<td>
+								<PriceHighlight variant='outcome'>- R$ 1.200,00</PriceHighlight>
+							</td>
+							<td>Casa</td>
+							<td>04/02/2023</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsTableContainer>
 		</div>
 	)
 }
